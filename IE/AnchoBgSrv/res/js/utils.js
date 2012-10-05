7a3dbc5be2a3f9c1dc4464e40558be9530a2a05e
Jan Kolomaznik <jan@salsitasoft.com>
2012-10-05 15:36:51 +0200

    Forgot to add utils.js source file

---
+++ IE/AnchoBgSrv/res/js/utils.js
@@ -0,0 +1,25 @@
+/******************************************************************************
+ * tabs.js
+ * Part of Ancho browser extension framework
+ * Implements aji.tabs
+ * Copyright 2012 Salsita software (http://www.salsitasoft.com).
+ ******************************************************************************/
+
+//******************************************************************************
+//* requires
+var Event = require("Event.js").Event;
+
+exports.EventFactory = {
+
+  createEvents : function(targetAPI, instanceID, apiName, eventNames) {
+    for (i = 0; i < eventNames.length; ++i) {
+      targetAPI[eventNames[i]] = new Event(apiName + '.' + eventNames[i], instanceID);
+    }
+  },
+
+  releaseEvents : function(instanceID, apiName, eventNames) {
+    for (i = 0; i < eventNames.length; ++i) {
+      addonAPI.removeEventObject(apiName + '.' + eventNames[i], instanceID);
+    }
+  }
+};
