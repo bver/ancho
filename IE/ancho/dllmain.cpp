76d2b94a84ccf0fc3b326a42df4f84aac255b8d4
Matthew Gertner <matthewgertner@Matthews-MacBook-Pro-2.local>
2012-12-13 12:00:57 +0100

    Fixed a few bugs to do with the PAPP and running content scripts.

--- IE/ancho/dllmain.cpp
+++ IE/ancho/dllmain.cpp
@@ -5,11 +5,48 @@
 #include "ancho_i.h"
 #include "dllmain.h"
 
+#include "ProtocolCF.h"
+#include "AnchoPassthruAPP.h"
+
 CanchoModule _AtlModule;
+CComPtr<IClassFactory> _CFHTTP;
+CComPtr<IClassFactory> _CFHTTPS;
+
+typedef PassthroughAPP::CMetaFactory<PassthroughAPP::CComClassFactoryProtocol,
+  CAnchoPassthruAPP> MetaFactory;
 
 // DLL Entry Point
 extern "C" BOOL WINAPI DllMain(HINSTANCE hInstance, DWORD dwReason, LPVOID lpReserved)
 {
   _AtlModule.m_hInstance = hInstance;
+  if (dwReason == DLL_PROCESS_ATTACH) {
+    // Register the Passthrough APP
+    CComPtr<IInternetSession> pInternetSession;
+    if (FAILED(CoInternetGetSession(0, &pInternetSession, 0))) {
+      return FALSE;
+    }
+
+    if (FAILED(MetaFactory::CreateInstance(CLSID_HttpProtocol, &_CFHTTP))) {
+      return FALSE;
+    }
+    if (FAILED(pInternetSession->RegisterNameSpace(_CFHTTP, CLSID_NULL, L"http", 0, 0, 0))) {
+      return FALSE;
+    }
+    if (FAILED(MetaFactory::CreateInstance(CLSID_HttpSProtocol, &_CFHTTPS))) {
+      return FALSE;
+    }
+    if (FAILED(pInternetSession->RegisterNameSpace(_CFHTTPS, CLSID_NULL, L"https", 0, 0, 0))) {
+      return FALSE;
+    }
+  }
+  else if (dwReason == DLL_PROCESS_DETACH) {
+    CComPtr<IInternetSession> pInternetSession;
+    if (FAILED(CoInternetGetSession(0, &pInternetSession, 0))) {
+      return FALSE;
+    }
+    pInternetSession->UnregisterNameSpace(_CFHTTP, L"http");
+    pInternetSession->UnregisterNameSpace(_CFHTTPS, L"https");
+  }
+
   return _AtlModule.DllMain(dwReason, lpReserved);
 }
